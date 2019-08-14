4f28eb79ca3cde3b22d02a25018f6e74a554f645
Lam1k <lamchik79@gmail.com>
2019-08-14 20:33:52 +0300

    Added googleMaps to Android and IOS

--- ios/Runner/AppDelegate.m
+++ ios/Runner/AppDelegate.m
@@ -1,11 +1,13 @@
 #include "AppDelegate.h"
 #include "GeneratedPluginRegistrant.h"
+#import "GoogleMaps/GoogleMaps.h"
 
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application
     didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
   [GeneratedPluginRegistrant registerWithRegistry:self];
+  [GMSServices provideAPIKey: @"AIzaSyBIm_t8KyBF6zxv71IzTn1p0jqdkbefgcc"];
   // Override point for customization after application launch.
   return [super application:application didFinishLaunchingWithOptions:launchOptions];
 }
