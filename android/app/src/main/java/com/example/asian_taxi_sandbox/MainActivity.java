d3a9fb4e9e400011fb14034f9105ac7413c0bb82
Yegor Pavlyukov <y.pavlyukov@newlinetechnologies.net>
2019-08-14 13:08:28 +0300

    init project

---
+++ android/app/src/main/java/com/example/asian_taxi_sandbox/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.asian_taxi_sandbox;
+
+import android.os.Bundle;
+import io.flutter.app.FlutterActivity;
+import io.flutter.plugins.GeneratedPluginRegistrant;
+
+public class MainActivity extends FlutterActivity {
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    GeneratedPluginRegistrant.registerWith(this);
+  }
+}
